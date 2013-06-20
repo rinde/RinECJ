385ccca4c360bbc018afe9feabe33399423ba455
rinde <Rinde.vanLon@cs.kuleuven.be>
2013-06-20 17:38:33 +0200

    added note about type parameter

--- src/main/java/rinde/jppf/ComputationTask.java
+++ src/main/java/rinde/jppf/ComputationTask.java
@@ -9,6 +9,8 @@ import org.jppf.server.protocol.JPPFTask;
  * @author Rinde van Lon <rinde.vanlon@cs.kuleuven.be>
  * 
  */
+// TODO investigate wether the second type parameter can be dropped (the data)
+// storing data in any kind of format should be entirely left to subclasses
 public abstract class ComputationTask<R extends GPComputationResult, C> extends JPPFTask {
 
 	protected final C taskData;
